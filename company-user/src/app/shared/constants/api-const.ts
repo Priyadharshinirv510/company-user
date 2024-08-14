7b9241f5d9c9543a1a8cb238331d950704b0fa07
Priyadharshini RV <priyarv2002@gmail.com>
2024-08-15 00:05:53 +0530

    added user edit

--- company-user/src/app/shared/constants/api-const.ts
+++ company-user/src/app/shared/constants/api-const.ts
@@ -4,4 +4,6 @@ export class APIConst{
     public static CREATE_COMPANY = "companies/createCompany"
     public static COMPANY_LIST = "companies/getCompanies"
     public static VIEW_USER = "users/getUserById/"
+    public static UPDATE_USER = 'companies/updateCompany'
+    public static DELETE_USER = 'users/deleteUser/:userId'
 }
